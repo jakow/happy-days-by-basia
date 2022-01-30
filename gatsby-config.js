b99e7dc20951786a57a0e8f3ea78b99f5396318e
Jakub Kowalczyk <j.a.kowalczyk1993@gmail.com>
2022-01-30 23:27:10 +0000

    Fix casing issue with Layout.tsx that prevented gatsby cloud from building

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,22 +23,22 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
+    // {
+    //   resolve: "gatsby-source-filesystem",
+    //   options: {
+    //     name: "images",
+    //     path: "./src/images/",
+    //   },
+    //   __key: "images",
+    // },
+    // {
+    //   resolve: "gatsby-source-filesystem",
+    //   options: {
+    //     name: "pages",
+    //     path: "./src/pages/",
+    //   },
+    //   __key: "pages",
+    // },
     "gatsby-plugin-postcss",
   ],
 };
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,8 @@ import Header from "./Header";
 type Props = {
   children: React.ReactNode;
 };
-export default function Layout({ children }) {
+
+export default function Layout({ children }: Props): React.ReactElement {
   return (
     <div className="h-screen bg-ivory">
       <Header />
