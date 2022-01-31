367ebb330e89ca0af541150e530f2accb82a1a64
Jakub Kowalczyk <j.a.kowalczyk1993@gmail.com>
2022-01-31 19:25:54 +0000

    add Helmet to pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,16 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import Layout from "../components/Layout"
+import * as React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/Layout";
+import Helmet from "react-helmet";
 
 export default function NotFoundPage(): React.ReactElement {
   return (
-    <Layout>404 Not found</Layout>
-  )
+    // TODO: design a 404 page.
+    <Layout>
+      <Helmet>
+        <title>Not Found</title>
+      </Helmet>
+      <div className="text-center p-2">Not found.</div>
+    </Layout>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
 import * as React from "react"
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image";
-
+import Helmet from 'react-helmet';
 
 export default function IndexPage(): React.ReactElement {
   return (
     <Layout>
+      <Helmet>
+        <title>Happy Days by Basia</title>
+      </Helmet>
       <div className="px-4 py-6">
-        <StaticImage className="w-full" src="../images/blogimg1.jpg" alt="First blog post" />
+        <StaticImage
+          className="w-full"
+          src="../images/blogimg1.jpg"
+          alt="First blog post"
+        />
       </div>
     </Layout>
   );
